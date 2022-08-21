769c5f39665e190930419b96e81bfdf01b54b94f
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-08-21 12:06:28 +0200

    URL toegevoegd van de tutorial

--- JS/Kalender.js
+++ JS/Kalender.js
@@ -1,12 +1,12 @@
 var cal = {
-  // (A) INIT CALENDAR
+  //INIT CALENDAR
   hMth:null, hYr:null, // MONTH & YEAR
   hWrap:null, // DAYS WRAPPER
   // EVENT FORM
   hBlock:null, hForm:null, hFormDel:null, hFormCX:null,
   hID:null, hStart:null, hEnd:null, hTxt:null, hColor:null,
   init : () => {
-    // (A1) GET HTML ELEMENTS
+    //GET HTML ELEMENTS
     cal.hMth = document.querySelector("#calmonth");
     cal.hYr = document.querySelector("#calyear");
     cal.hWrap = document.querySelector("#calwrap");
@@ -20,36 +20,36 @@ var cal = {
     cal.hTxt = document.querySelector("#evttxt");
     cal.hColor = document.querySelector("#evtcolor");
 
-    // (A2) ATTACH CONTROLS
+    //ATTACH CONTROLS
     cal.hMth.onchange = cal.draw;
     cal.hYr.onchange = cal.draw;
     cal.hForm.onsubmit = cal.save;
     cal.hFormDel.onclick = cal.del;
     cal.hFormCX.onclick = cal.hide;
 
-    // (A3) DRAW CURRENT MONTH/YEAR
+    //DRAW CURRENT MONTH/YEAR
     cal.draw();
   },
 
-  // (B) SUPPORT FUNCTION - AJAX FETCH
+  //SUPPORT FUNCTION - AJAX FETCH
   ajax : (data, load) => {
     fetch("./ajax/KalenderAjax.php", { method:"POST", body:data })
     .then(res=>res.text()).then(load);
   },
 
-  // (C) DRAW CALENDAR
+  //DRAW CALENDAR
   draw : () => {
-    // (C1) FORM DATA
+    //FORM DATA
     let data = new FormData();
     data.append("req", "draw");
     data.append("month", cal.hMth.value);
     data.append("year", cal.hYr.value);
-    // (C2) AJAX LOAD SELECTED MONTH
+    //AJAX LOAD SELECTED MONTH
     cal.ajax(data, (res) => {
-      // (C2-1) ATTACH CALENDAR TO WRAPPER
+      //ATTACH CALENDAR TO WRAPPER
       cal.hWrap.innerHTML = res;
 
-      // (C2-2) CLICK DAY CELLS TO ADD EVENT
+      //CLICK DAY CELLS TO ADD EVENT
       for (let day of cal.hWrap.getElementsByClassName("day")) {
         day.onclick = () => { cal.show(day); };
       }
@@ -61,11 +61,11 @@ var cal = {
     });
   },
 
-  // (D) SHOW EVENT FORM
+  //SHOW EVENT FORM
   show : (cell) => {
     let eid = cell.getAttribute("data-eid");
 
-    // (D1) ADD NEW EVENT
+    //ADD NEW EVENT
     if (eid === null) {
       let y = cal.hYr.value, m = cal.hMth.value, d = cell.dataset.day;
       if (m.length==1) { m = "0" + m; }
@@ -78,7 +78,7 @@ var cal = {
       cal.hFormDel.style.display = "none";
     }
 
-    // (D2) EDIT EVENT
+    //EDIT EVENT
     else {
       let edata = JSON.parse(document.getElementById("evt"+eid).innerHTML);
       cal.hID.value = eid;
@@ -89,14 +89,14 @@ var cal = {
       cal.hFormDel.style.display = "block";
     }
 
-    // (D3) SHOW EVENT FORM
+    //SHOW EVENT FORM
     cal.hBlock.classList.add("show");
   },
 
-  // (E) HIDE EVENT FORM
+  //HIDE EVENT FORM
   hide : () => { cal.hBlock.classList.remove("show"); },
 
-  // (F) SAVE EVENT
+  //SAVE EVENT
   save : () => {
     cal.ajax(new FormData(cal.hForm), (res) => {
       if (res=="OK") { cal.hide(); cal.draw(); }
@@ -105,14 +105,14 @@ var cal = {
     return false;
   },
 
-  // (G) DELETE EVENT
+  //DELETE EVENT
   del : () => { if (confirm("Delete Event?")) {
     // (G1) FORM DATA
     let data = new FormData();
     data.append("req", "del");
     data.append("eid", cal.hID.value);
 
-    // (G2) AJAX DELETE
+    //AJAX DELETE
     cal.ajax(data, (res) => {
       if (res=="OK") { cal.hide(); cal.draw(); }
       else { alert(res); }
@@ -120,3 +120,7 @@ var cal = {
   }}
 };
 window.addEventListener("DOMContentLoaded", cal.init);
+
+/*
+  We hebben deze tutorial gevolgt: https://code-boxx.com/simple-php-calendar-events/ 
+*/
