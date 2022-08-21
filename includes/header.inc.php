d2a7362f185a8a7008271c6ca68cf24c8176577e
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-08-21 10:59:45 +0200

    golive in nav voor admin account

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,23 +5,12 @@
 <body>
     <nav>
         <a href="startpagina.php">LightPath</a>
-        <a href="start.php">Home</a>
+        <a href="index.php">Home</a>
         <a href="live.php">Live</a>
         <a href="posts.php">Gemeenschap</a>
         <a href="kalender.php">Kalender</a>
-        <a href="contact2.php">Contacten</a>
-        <?php
-        if(!empty($_SESSION)):
-        ?>
-        <a href="profile.php" class="rightNav">Profiel</a>
-        <a href="logout.php" class="rightNav">Uitloggen</a>
-        <?php
-        elseif(1 == 1):
-        ?>
-        <a href="login.php" class="rightNav">Inloggen</a>
-        <?php
-        endif;
-        ?>
+        <a href="">Contacten</a>
+        <a href="">Profiel</a>
     </nav>
 </body>
 </html>
