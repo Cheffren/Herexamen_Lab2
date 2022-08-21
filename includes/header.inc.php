14d47797a2156cc1ede4bca586a9d1fe26505fbf
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-08-21 17:03:03 +0200

    nav aangepast

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,12 +5,32 @@
 <body>
     <nav>
         <a href="startpagina.php">LightPath</a>
-        <a href="index.php">Home</a>
+        <a href="start.php">Home</a>
         <a href="live.php">Live</a>
+        <?php
+            $checkAdmin = new CheckAdminLogIn;
+            if($checkAdmin->checkAdmin() === true):
+        ?>
+        <a href="goLive.php">Go Live</a>
+        <?php
+            endif;
+        ?>
         <a href="posts.php">Gemeenschap</a>
         <a href="kalender.php">Kalender</a>
-        <a href="">Contacten</a>
-        <a href="">Profiel</a>
+        <a href="contact.php">Contacten</a>
+        <?php
+        if(!empty($_SESSION)):
+        ?>
+        <div class="rightNav"></div>
+        <a href="profile.php" >Profiel</a>
+        <a href="logout.php">Uitloggen</a>
+        <?php
+        elseif(1 == 1):
+        ?>
+        <a href="login.php" class="rightNav">Inloggen</a>
+        <?php
+        endif;
+        ?>
     </nav>
 </body>
 </html>
