0c1a94cba9f743dc5c594752a2ed60c6cd3da229
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-08-21 09:45:58 +0200

    login+logout added to nav

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,8 +9,19 @@
         <a href="live.php">Live</a>
         <a href="posts.php">Gemeenschap</a>
         <a href="kalender.php">Kalender</a>
-        <a href="">Contacten</a>
-        <a href="" class="profileLink">Profiel</a>
+        <a href="contact2.php">Contacten</a>
+        <?php
+        if(!empty($_SESSION)):
+        ?>
+        <a href="profile.php" class="rightNav">Profiel</a>
+        <a href="logout.php" class="rightNav">Uitloggen</a>
+        <?php
+        elseif(1 == 1):
+        ?>
+        <a href="login.php" class="rightNav">Inloggen</a>
+        <?php
+        endif;
+        ?>
     </nav>
 </body>
 </html>
